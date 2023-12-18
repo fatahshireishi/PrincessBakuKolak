7f76679c93aa42a28ca3d17ca04010bf89bc6db8
thisisfatah <48764326+thisisfatah@users.noreply.github.com>
2023-12-18 12:34:57 +0700

    Squashed commit of the following:
    commit b1d8c259214db7a0d9abf9a13e07a185fea3d181
    Author: thisisfatah <48764326+thisisfatah@users.noreply.github.com>
    Date:   Mon Dec 18 12:34:07 2023 +0700
    
        Update .gitignore
    
    commit a42b9fe6ef64ad1d8b6c7e98bff1062266044deb
    Author: thisisfatah <48764326+thisisfatah@users.noreply.github.com>
    Date:   Mon Dec 18 12:32:04 2023 +0700
    
        Type Mechanic
    
        adding new script mechanic type

---
+++ Assets/Scripts/WordManager/WordDisplay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordDisplay : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+
+    public void SetWord(string word)
+    {
+        text.text = word;
+    }
+
+    public void RemoveLetter()
+    {
+        text.text = text.text.Remove(0, 1);
+        text.color = Color.red;
+    }
+
+    public void RemoveWord()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WordManager/WordManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour
+{
+    public List<Word> words;
+
+    bool hasActiveWord;
+    Word activeWord;
+
+    [SerializeField] int wordCount = 1;
+
+    WordSpawner spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner = GetComponent<WordSpawner>();
+        for (int i = 0; i < wordCount; i++)
+        {
+            AddWord();
+        }
+    }
+
+    private void AddWord()
+    {
+        Word word = new Word(WordGenerator.GetRandomWord(), spawner.SpawnWord());
+
+        Debug.Log(word.word);
+        words.Add(word);
+    }
+
+    public void TypeLatter(char letter)
+    {
+        if (hasActiveWord)
+        {
+            if (activeWord.GetNextLetter() == letter)
+            {
+                activeWord.TypeLetter();
+            }
+        }
+        else
+        {
+            foreach (Word word in words)
+            {
+                if (word.GetNextLetter() == letter)
+                {
+                    activeWord = word;
+                    hasActiveWord = true;
+                    word.TypeLetter();
+                    break;
+                }
+            }
+        }
+
+        if(hasActiveWord && activeWord.WordTyped())
+        {
+            hasActiveWord = false;
+            words.Remove(activeWord);
+        }
+    }
+}
